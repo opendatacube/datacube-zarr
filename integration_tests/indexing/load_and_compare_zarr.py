cbe3f25be20d85927ab6a1968a0a99a76ac909cd
Dave Cole <david.cole@data61.csiro.au>
2020-05-13 16:21:15 +1000

    Bash script for LS5 convert->prepare->index->load integration test

---
+++ integration_tests/indexing/load_and_compare_zarr.py
@@ -0,0 +1,30 @@
+#! /usr/bin/env python3
+
+import datacube
+
+# LS5 NBAR scene params
+output_crs = "EPSG:28355"
+resolution = (25, -25)
+latitude = (-35.282468, -35.315409)
+longitude = (149.0689, 149.156705)
+
+# Load data
+dc = datacube.Datacube()
+data_tiff = dc.load(
+    product='ls5_nbar_scene',
+    latitude=latitude,
+    longitude=longitude,
+    output_crs=output_crs,
+    resolution=resolution
+)
+data_zarr = dc.load(
+    product='ls5_nbar_scene_zarr',
+    latitude=latitude,
+    longitude=longitude,
+    output_crs=output_crs,
+    resolution=resolution
+)
+
+# compare datasets
+assert data_zarr.equals(data_tiff)
+print(data_zarr)
