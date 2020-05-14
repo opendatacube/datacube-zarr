ca517966d5052ec76b3b4b850ce70ea42e43fb32
Dave Cole <david.cole@data61.csiro.au>
2020-05-14 14:13:51 +1000

    Make convert.py generic to simply convert tif to zarr

--- utils/convert.py
+++ utils/convert.py
@@ -1,27 +1,91 @@
 #! /usr/bin/env python
 
-import re
-from datetime import datetime
 from pathlib import Path
-from typing import Any, Dict, Generator, List, Optional, Tuple
-from xml.etree import ElementTree
+from typing import Any, Callable, List, Optional, Tuple
 
 import click
 import xarray as xr
-from dateutil.parser import parse as date_parse
 from s3path import S3Path
 
 from zarr_io import ZarrIO
 
+_GEOTIFFS = (".tif", ".tiff", ".gtif")
+_DATA_FILES = [x for t in (_GEOTIFFS,) for x in t]
+
+
+def path_as_str(path: Path) -> str:
+    """Convert Path to string and S3Path to URI."""
+    s = path.as_uri()
+    if s.startswith("file://"):
+        s = s[7:]
+    return s
+
+
+def save_dataset_to_zarr(ds: xr.Dataset, root: Path, group: str, **kwargs: Any) -> None:
+    """Save an xarray dataset to s3 or file in Zarr format."""
+    protocol = "s3" if root.as_uri().startswith("s3://") else "file"
+    zio = ZarrIO(protocol)
+    root_str = path_as_str(root)
+    zio.save_dataset(root=root_str, group_name=group, dataset=ds, **kwargs)
+    print(f"create: {root_str}/{group}")
+
+
+def convert_dir(in_dir: Path, out_dir: Optional[Path] = None, **zarrgs: Any) -> None:
+    """Recursively convert datasets in a directory to Zarr format."""
+    assert in_dir.is_dir()
+    sub_paths = [p for p in in_dir.iterdir() if p.relative_to(in_dir).name]
+    for p in sub_paths:
+        out_p = out_dir / p.name if out_dir else None
+        if p.is_dir():
+            convert_dir(p, out_p)
+        elif p.suffix in _DATA_FILES:
+            convert_to_zarr(p, out_dir, **zarrgs)
+        elif out_p is not None:
+            out_p.write_bytes(p.read_bytes())
+
+
+def convert_to_zarr(in_file: Path, out_dir: Optional[Path] = None, **zarrgs: Any) -> None:
+    """Convert file to Zarr format."""
+    inplace = out_dir is None
+    if out_dir is None:
+        out_dir = in_file.parent
+
+    if in_file.suffix in _GEOTIFFS:
+        raster_to_zarr(in_file, out_dir, **zarrgs)
+    else:
+        raise ValueError(f"Unsupported data file format: {in_file.suffix}")
+
+    # if converting inplace, remove the original file
+    if inplace:
+        if in_file.as_uri().startswith("s3://"):
+            in_file.rmdir()
+        else:
+            in_file.unlink()
+        print(f"delete: {path_as_str(in_file)}")
+
+
+def raster_to_zarr(raster: Path, out_dir: Path, **zarrgs: Any) -> None:
+    """Convert a raster file to Zarr."""
+    da = xr.open_rasterio(raster.as_uri())
+    ds = da.to_dataset(name="array")
+    save_dataset_to_zarr(ds, out_dir, group=raster.stem, **zarrgs)
+
+
+# CLI functions
 
 class KeyValue(click.ParamType):
     """A click param for any key/value pairs."""
 
-    name = "KeyValue"
+    name = "Key:Value"
 
-    def __init__(self, key_type: type = str, val_type: type = str, sep: str = ":"):
-        self.key_type = key_type
-        self.val_type = val_type
+    def __init__(
+        self,
+        key: Callable[[str], Any] = str,
+        value: Callable[[str], Any] = str,
+        sep: str = ":"
+    ):
+        self.key_fn = key
+        self.value_fn = value
         self.sep = sep
 
     def convert(
@@ -29,7 +93,7 @@ class KeyValue(click.ParamType):
     ) -> Tuple:
         """Convert key:valueto tuple."""
         k, v = value.split(self.sep, 1)
-        return self.key_type(k), self.val_type(v)
+        return self.key_fn(k), self.value_fn(v)
 
 
 class FileOrS3Path(click.ParamType):
@@ -45,9 +109,11 @@ class FileOrS3Path(click.ParamType):
     ) -> Path:
         """Convert file/s3 path str to pathlib Path."""
         if value.startswith("s3:/"):
-            path = S3Path(value[4:])
+            path: Path = S3Path(value[4:])
         else:
-            path = Path(value)
+            if value.startswith("file://"):
+                value = value[7:]
+            path = Path(value).resolve()
 
         if self.exists and not path.exists:
             raise ValueError(f"{path.as_uri()} does not exist.")
@@ -55,118 +121,35 @@ class FileOrS3Path(click.ParamType):
         return path
 
 
-def save_dataset_to_zarr(
-    ds: xr.Dataset, root: Path, group: str, **kwargs: Any
-) -> None:
-    """Save an xarray dataset to s3 or file in Zarr format."""
-    if root.as_uri().startswith("s3://"):
-        protocol = "s3"
-        root_ = root.as_uri()
-    else:
-        protocol = "file"
-        root_ = str(root)
+def check_options(outpath: Path, inplace: bool) -> None:
+    """Some checks on command inputs."""
+    if not outpath and not inplace:
+        raise click.UsageError("--inplace flag is required if --outpath is not set.")
 
-    zio = ZarrIO(protocol)
-    zio.save_dataset(root=root_, group_name=group, dataset=ds, **kwargs)
-
-
-def convert_ls5_scene(
-    scene_path: Path, zarr_dir: Path, chunks: Optional[Dict[str, int]]
-) -> None:
-    """Convert scene into Zarr format."""
-    group = scene_path.parts[-1]
-    meta, scene_attr = get_ls5_metadata(scene_path)
-    scene_time = scene_attr.pop("time")
-
-    darrays: Dict[str, xr.DataArray] = {}
-    tifs = sorted(scene_path.glob("**/*.tif"))
-    for t in tifs:
-        da = xr.open_rasterio(t.as_uri())
-
-        # Drop band coordinate (assumes all tifs have only 1 band)
-        da = da.squeeze(drop=True)
-        assert da.ndim == 2
-
-        # Add time coordinate
-        da = da.expand_dims(dim={"time": [scene_time]}, axis=0)
-
-        # Add nodata value
-        da.attrs["nodata"] = da.nodatavals[0]
-        da.attrs.update(scene_attr)
-
-        # Get a band name from file
-        band = get_ls5_band_name(t.stem)
-        assert band not in darrays
-        darrays[band] = da
-
-    # Save dataset
-    ds = xr.Dataset(darrays)
-    save_dataset_to_zarr(ds, zarr_dir, group, chunks=chunks)
-
-    # Copy metadata
-    meta_cp = zarr_dir / meta.name
-    assert meta_cp.parent.exists()
-    meta_cp.write_text(meta.read_text())
-
-
-def get_ls5_band_name(name: str):
-    """Extract landsat band name from geotif filename."""
-    i = name.rfind('_')
-    if i == -1:
-        raise ValueError(f"Unexpected tif image in eods: {name}")
-    if re.match(r"[Bb]\d+", name[i + 1:]):
-        band = name[i + 2:i + 3]
-    elif name[i + 1:].startswith('1111111111111100'):
-        band = 'pqa'
-    else:
-        band = name[i + 1:]
-    return band
-
-
-def get_ls5_metadata(scene_path: Path) -> Tuple[Path, Dict]:
-    """Copy metadata file and extract scene time."""
-
-    def _find_datetime(tree: ElementTree.Element, path: str) -> datetime:
-        """Find a datetime in an XML tree."""
-        elem = tree.find("./EXEXTENT/TEMPORALEXTENTFROM")
-        if elem is not None:
-            dt = date_parse(str(elem.text))
-        else:
-            raise ValueError("Could not find datetime within 'metadata.xml'.")
-        return dt
-
-    try:
-        meta = next(scene_path.glob("metadata.xml"))
-    except StopIteration:
-        raise FileNotFoundError(f"Can't find metadata.xml within {scene_path}.")
-
-    tree = ElementTree.fromstring(meta.read_text())
-    start_time = _find_datetime(tree, "./EXEXTENT/TEMPORALEXTENTFROM")
-    end_time = _find_datetime(tree, "./EXEXTENT/TEMPORALEXTENTTO")
-    attr = {"time": start_time + (end_time - start_time) // 2}
-    return meta, attr
-
-
-def get_ls5_dataset_dirs(p: Path) -> Generator[Path, None, None]:
-    """Find directories with 'metadata.xml' file."""
-    subp = [s for s in p.iterdir() if s.relative_to(p).name]
-    if p / "metadata.xml" in subp:
-        yield p
-    else:
-        yield from (p for s in subp if s.is_dir() for p in get_ls5_dataset_dirs(s))
+    if outpath and inplace:
+        raise click.UsageError("Can not set both --outpath and --inplace options.")
 
 
 @click.command()
 @click.argument("dataset", type=FileOrS3Path(exists=True), required=True)
-@click.option("--outdir", type=FileOrS3Path(), required=True)
-@click.option("--chunks", type=KeyValue(val_type=int), multiple=True)
-def main(dataset: Path, outdir: Path, chunks: Optional[List[Tuple[str, int]]]) -> None:
-    """Convert (LS5) datasets to Zarr format."""
-    chunks_ = dict(chunks) if chunks else None
-    for d in get_ls5_dataset_dirs(dataset):
-        outdir_ = outdir / d.stem
-        convert_ls5_scene(d, outdir_, chunks=chunks_)
-        print(outdir_)
+@click.option(
+    "--outpath", type=FileOrS3Path(), required=False,
+    help="Path to save the converted dataset directory."
+)
+@click.option(
+    "--chunk", type=KeyValue(value=int), multiple=True,
+    help="Zarr chunk option '<dim>:<size>'."
+)
+@click.option(
+    "--inplace", is_flag=True, help="Convert inplace (deletes original data files)."
+)
+def main(
+    dataset: Path, outpath: Path, inplace: bool, chunk: Optional[List[Tuple[str, int]]]
+) -> None:
+    """Convert datasets to Zarr format."""
+    check_options(outpath, inplace)
+    chunks = dict(chunk) if chunk else None
+    convert_dir(dataset, outpath, chunks=chunks)
 
 
 if __name__ == "__main__":
