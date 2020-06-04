b01f6b1dfb75b0f7977c7c957cdcad97d68be021
Dave Cole <david.cole@data61.csiro.au>
2020-06-04 14:04:30 +1000

    Update uri doc

--- zarr_io/utils/uris.py
+++ zarr_io/utils/uris.py
@@ -7,8 +7,10 @@ def uri_split(uri: str) -> Tuple[str, str, str]:
     Splits uri into protocol, root, and group
 
     Example:
-        uri_split('file:///path/to/my_dataset.zarr#group')
-        returns ('file', '/path/to/my_dataset.zarr', 'group')
+        uri_split('file:///path/to/my_dataset.zarr#group/subgroup/etc')
+        returns ('file', '/path/to/my_dataset.zarr', 'group/subgroup/etc')
+
+    If the URI contains no '#' extension, the root group "" is returned.
 
     :param str uri: The URI to be parsed
     :return: (protocol, root, group)
