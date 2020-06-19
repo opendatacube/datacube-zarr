c25ef25fd935eea930eec05ef92df972ed4d2bff
Dave Cole <david.cole@data61.csiro.au>
2020-06-19 15:04:47 +1000

    Update deps

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ setup(
     description="Zarr plug-in driver for datacube",
     long_description=open('readme.md').read(),
     long_description_content_type='text/markdown',
-    python_requires='>=3.6',
+    python_requires='>=3.6.0',
     packages=find_packages(
         exclude=(
             'tests', 'tests.*',
@@ -36,12 +36,12 @@ setup(
     },
     setup_requires=['setuptools_scm'],
     install_requires=[
-        'boto3>=1.9',
+        'boto3>=1.9.0',
         'datacube>1.8.0',
         'numcodecs>=0.6.2',
         'rasterio>=1.0.4',
         's3path>=0.1.93',
-        's3fs>=0.2',
+        's3fs>=0.2.0',
         'xarray>=0.14.1',
         'zarr>=2.3.2',
     ],
