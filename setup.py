6200b8d7ee30801cb49aba2403aa12afb5082c3f
Dave Cole <david.cole@data61.csiro.au>
2020-06-19 13:17:43 +1000

    Add datacube dependency

--- setup.py
+++ setup.py
@@ -37,6 +37,7 @@ setup(
     setup_requires=['setuptools_scm'],
     install_requires=[
         'boto3>=1.9',
+        'datacube>1.8.0',
         'numcodecs>=0.6.2',
         'rasterio>=1.0.4',
         's3path>=0.1.93',
