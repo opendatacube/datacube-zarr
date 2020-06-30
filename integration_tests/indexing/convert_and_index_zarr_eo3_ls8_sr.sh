a7bc5d35e09e7ee550d6f88ceec7167deec72696
Dave Cole <david.cole@data61.csiro.au>
2020-06-30 12:49:16 +1000

    Test script for converting/indexing zarr with EO3

---
+++ integration_tests/indexing/convert_and_index_zarr_eo3_ls8_sr.sh
@@ -0,0 +1,43 @@
+#! /usr/bin/env bash
+
+# Example commands to:
+# 1) Prepare and index LS8 SR geotif
+# 2) Convert geotifs to zarr
+# 3) Prepare and index LS8 SR zarr
+# 4) Compare with original geotiff data
+
+set -x
+
+# create tmp dir
+TMP_DIR=/root/data/tmp/eo3_test
+[ -d $TMP_DIR ] && rm -r $TMP_DIR
+mkdir -p $TMP_DIR
+
+# Clean db
+python3 /usr/local/bin/db_initialiser.py delete
+python3 /usr/local/bin/db_initialiser.py create
+datacube system init
+datacube system check
+
+# Repos
+BASEDIR=/root/okteto
+DATACUBE_DIR=$BASEDIR/datacube-core
+DATACUBE_DRIVER_DIR=$BASEDIR/datacube-drivers
+EO3_ASSEMBLE_DIR=$BASEDIR/eo3-assemble
+DATA_PIPELINE_DIR=$BASEDIR/data-pipeline
+
+# Geotiff test data
+LS8_SR_TEST_DATA=$DATACUBE_DRIVER_DIR/tests/data/espa/ls8_sr_2
+cp -r $LS8_SR_TEST_DATA $TMP_DIR
+LS8_SR_TEST_DATA_TMP=$TMP_DIR/$(basename $LS8_SR_TEST_DATA)
+LS8_SR_GTIF_PRODUCT=$DATA_PIPELINE_DIR/live_ingester/source_drivers/espa/definitions/product/usgs_espa_ls8c1_sr.yaml
+
+# Add/index the original LS5 GeoTiff product
+datacube product add $LS8_SR_GTIF_PRODUCT
+python $EO3_ASSEMBLE_DIR/eo3prepare_usgs_espa_ls8c1_l2.py -p $LS8_SR_GTIF_PRODUCT $LS8_SR_TEST_DATA_TMP
+datacube dataset add $LS8_SR_TEST_DATA_TMP/odc-metadata.yaml
+
+# Convert GeoTiff dataset to zarr
+zarrify --outpath $TMP_DIR/zarr --chunk x:500 --chunk y:500 --ignore "*.tar.gz,*.md5" $LS8_SR_TEST_DATA_TMP
+
+# Add/index the LS5 zarr product
