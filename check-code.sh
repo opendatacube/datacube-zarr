5a2217cd29a1fcf2a8d7c74d5d324dafb1540bfc
Wang <wan064@csiro.au>
2020-07-08 11:39:01 +1000

    Tests for retry decorator Output coverage report to terminal

--- check-code.sh
+++ check-code.sh
@@ -13,5 +13,6 @@ black --skip-string-normalization --line-length 90 --check .
 
 # Run tests, taking coverage.
 # Users can specify extra folders as arguments.
-pytest -r a --cov-report html:coverage/htmlcov --cov-report xml:coverage/cov.xml \
-  --cov datacube_zarr --doctest-ignore-import-errors --durations=5 datacube_zarr tests $@
+pytest -r a --cov-report term-missing --cov-report html:coverage/htmlcov \
+  --cov-report xml:coverage/cov.xml --cov datacube_zarr --doctest-ignore-import-errors \
+  --durations=5 datacube_zarr tests $@
