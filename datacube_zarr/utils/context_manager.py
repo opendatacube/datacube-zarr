72674fcfbfa439d63fe26a36d198ea4d2afd761d
Wang <wan064@csiro.au>
2020-08-19 14:04:32 +1000

    Cleaner version of dask_threadsafe_config.

--- datacube_zarr/utils/context_manager.py
+++ datacube_zarr/utils/context_manager.py
@@ -1,11 +1,11 @@
 from contextlib import ExitStack, contextmanager
-from typing import Iterator, Union
+from typing import Iterator
 
 from dask import config
 
 
 @contextmanager
-def dask_threadsafe_config(protocol: str) -> Iterator[Union[config.set, ExitStack]]:
+def dask_threadsafe_config(protocol: str) -> Iterator[ExitStack]:
     """
     Creates a single worker Dask context if the `protocol` is `s3`
     and the Dask `scheduler` is `threaded`, otherwise unchanged.
@@ -15,11 +15,11 @@ def dask_threadsafe_config(protocol: str) -> Iterator[Union[config.set, ExitStac
     :param protocol: The storage URI.
     :return: thread-safe contextmanager
     """
-    # Set num_workers to 1 due to a thread-safety issue with moto + s3fs
-    if protocol == 's3' and config.config.get('scheduler', None) in [
-        'threads',
-        None,
-    ]:
-        yield config.set(num_workers=1)
-    else:
-        yield ExitStack()
+    with ExitStack() as stack:
+        # Set num_workers to 1 due to a thread-safety issue with moto + s3fs
+        if protocol == 's3' and config.config.get('scheduler', None) in [
+            'threads',
+            None,
+        ]:
+            stack.enter_context(config.set(num_workers=1))
+        yield stack
