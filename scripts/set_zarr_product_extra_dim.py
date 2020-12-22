9b5bd49bc99fc539ce084994a34751cbdc70107b
Dave Cole <david.cole@data61.csiro.au>
2020-12-22 16:01:20 +1100

    Add support for multiple extra dimensions to rename script

--- scripts/set_zarr_product_extra_dim.py
+++ scripts/set_zarr_product_extra_dim.py
@@ -7,32 +7,46 @@ import numpy as np
 import xarray as xr
 import yaml
 
-from datacube_zarr.tools.zarrify import FileOrS3Path
+from datacube_zarr.tools.zarrify import FileOrS3Path, KeyValue
 from datacube_zarr.zarr_io import replace_dataset_dim
+
+_default_zarrify_dim = "band"
 
 
 @click.command()
 @click.argument("zarr", type=FileOrS3Path(exists=True), required=True)
 @click.argument("product", type=FileOrS3Path(exists=True), required=True)
-def cli(zarr, product):
+@click.option(
+    "--dim-map",
+    type=KeyValue(),
+    multiple=True,
+    help="Dimension name mapping '<old>:<new>'.",
+)
+def cli(zarr, product, dim_map):
     """Replace zarr "band" dimension with product specific extra dimension."""
 
     pd = yaml.load(product.read_text(), Loader=yaml.SafeLoader)
-    ed = pd["extra_dimension"]
-    # Todo: Do this properly
-    ed = ed[0]
-    dim = xr.IndexVariable(ed["name"], np.array(ed["values"], dtype=ed["dtype"]))
+    eds = pd["extra_dimensions"]
+    ed_names = [ed["name"] for ed in eds]
+    assert len(ed_names) == len(set(ed_names))
 
-    # Get number of bands / length of z dim
-    band_meta_file = zarr / "band/.zarray"
-    n = json.loads(band_meta_file.read_text())["shape"][0]
+    replace_dims = {old: eds[ed_names.index(new)] for old, new in dim_map.items()}
+    if not replace_dims:
+        replace_dims[_default_zarrify_dim] = eds[0]
 
-    if n != len(dim):
-        raise ValueError(
-            f"Inconsistent dimension lengths: band {n}, {ed['name']} {len(dim)}."
-        )
+    for oname, ed in replace_dims.items():
+        dim = xr.IndexVariable(ed["name"], np.array(ed["values"], dtype=ed["dtype"]))
 
-    replace_dataset_dim(zarr.as_uri(), "band", dim)
+        # Get number of bands / length of z dim
+        band_meta_file = zarr / oname / ".zarray"
+        n = json.loads(band_meta_file.read_text())["shape"][0]
+
+        if n != len(dim):
+            raise ValueError(
+                f"Inconsistent dimension lengths: {oname} {n}, {ed['name']} {len(dim)}."
+            )
+
+        replace_dataset_dim(zarr.as_uri(), oname, dim)
 
 
 if __name__ == "__main__":
