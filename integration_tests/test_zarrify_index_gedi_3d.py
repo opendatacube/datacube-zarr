bc2b805b0465a6531006c1a9d83853348effcb75
Dave Cole <david.cole@data61.csiro.au>
2021-02-25 17:37:24 +1100

    GEDI 3D unit tests

--- integration_tests/test_zarrify_index_gedi_3d.py
+++ integration_tests/test_zarrify_index_gedi_3d.py
@@ -31,6 +31,10 @@ def _gedi_product_is_3d(prod):
     return prod.endswith("_z")
 
 
+def custom_dumb_fuser(dst, src):
+    dst[:] = src[:]
+
+
 GEDI_RESOLUTION = (0.00027778, -0.00027778)
 GEDI_CRS = "EPSG:4326"
 
@@ -39,10 +43,10 @@ LBG_LATITUDE = (-35.821784394017975, -34.988444394017975)
 LBG_LONGITUDE = (148.64089852640407, 149.75201852640407)
 
 
-@pytest.fixture
-def gedi_zarr3d(tmp_path):
+@pytest.fixture(scope="session")
+def gedi_zarr3d(tmp_path_factory):
     """Zarrify GEDI rasters"""
-    zarr_dir = tmp_path / "zarrs"
+    zarr_dir = tmp_path_factory.mktemp("gedi_3d_zarrs")
     runner = CliRunner()
     zarrify_args = ["--chunk", "x:512", "--chunk", "y:512", "--progress"]
     zarrify_args.extend(["--multi-dim", "--outpath", str(zarr_dir)])
@@ -123,6 +127,7 @@ def test_gedi_gtif_index(indexed_gedi_gtif, index):
         resolution=GEDI_RESOLUTION,
     )
     assert list(data.sizes.values()) == [2487, 3396, 2]
+    assert not (data['pai'].values == data['pai'].nodata).all()
 
 
 def test_gedi_zarr_index(indexed_gedi_zarr, index):
@@ -142,6 +147,17 @@ def test_gedi_zarr_index(indexed_gedi_zarr, index):
     )
     # zarr metadata doesnt include geometry.coordinates for valid region
     assert list(data.sizes.values()) == [3011, 4195, 2]
+    assert not (data['pai'].values == data['pai'].nodata).all()
+
+
+def stack_3d_on_z(ds, name):
+    """Stack 'z' measurments together along new 3D 'z' dimension."""
+    da = ds.to_array(dim="z")
+    da = da.assign_coords(z=da.z.astype(float))
+    dim_order = ['time', 'z', *da.dims[-2:]]
+    da = da.transpose(*dim_order)
+    ds3 = da.to_dataset(name=name)
+    return ds3
 
 
 def test_gedi_gtif_zarr_product(index, indexed_gedi_gtif, indexed_gedi_zarr):
@@ -166,11 +182,13 @@ def test_gedi_gtif_zarr_product(index, indexed_gedi_gtif, indexed_gedi_zarr):
             output_crs=GEDI_CRS,
             resolution=GEDI_RESOLUTION,
         )
+        for da in data_zarr.data_vars.values():
+            assert da.size
 
         # compare datasets
         if not _gedi_product_is_3d(prod):
             # For 2D products, outputs are equal
-            assert data_zarr.equals(data_tiff)
+            xr.testing.assert_equal(data_zarr, data_tiff)
 
         else:
             # For 3d products all measurements indludes 2D measurements for each value in
@@ -179,10 +197,140 @@ def test_gedi_gtif_zarr_product(index, indexed_gedi_gtif, indexed_gedi_zarr):
             # Compare 2D measurements with gtiff product
             measurement = prod[len("gedi_l2b_") :]
             data_zarr_2d = data_zarr.drop_vars([measurement, "z"])
-            assert data_zarr_2d.equals(data_tiff)
+            xr.testing.assert_equal(data_zarr_2d, data_tiff)
 
-            # Compare the 3d measurement (by slicing each value)
+            # Merge the tiff 2d measurement to 3d and compare to zarr
             data_zarr_3d = data_zarr[measurement]
-            for z, d_tiff in data_tiff.data_vars.items():
-                d_zarr = data_zarr_3d.sel(z=int(z)).drop_vars("z").rename(z)
-                assert d_zarr.equals(d_tiff)
+            data_tiff_3d = stack_3d_on_z(data_tiff, measurement)[measurement]
+            xr.testing.assert_equal(data_tiff_3d, data_zarr_3d)
+
+
+def test_3d_reprojection(index, indexed_gedi_gtif, indexed_gedi_zarr):
+    """Reproject to GDA94 (MGA Zone 55)."""
+    dc = Datacube(index=index)
+    prod = "gedi_l2b_cover_z"
+    measurement = "cover_z"
+    output_crs = "EPSG:28355"
+    resolution = (25, -25)
+
+    resampling_methods = [
+        'nearest',
+        'cubic',
+        'bilinear',
+        'cubic_spline',
+        'lanczos',
+        'average',
+    ]
+
+    for resampling in resampling_methods:
+
+        data_tiff = dc.load(
+            product=prod,
+            latitude=LBG_LATITUDE,
+            longitude=LBG_LONGITUDE,
+            output_crs=output_crs,
+            resolution=resolution,
+            resampling=resampling,
+        )
+        data_tiff_3d = stack_3d_on_z(data_tiff, measurement)
+
+        data_zarr = dc.load(
+            product=f"{prod}_zarr",
+            measurements=[measurement],
+            latitude=LBG_LATITUDE,
+            longitude=LBG_LONGITUDE,
+            output_crs=output_crs,
+            resolution=resolution,
+            resampling=resampling,
+        )
+        assert data_zarr[measurement].size
+
+        if resampling in ("nearest",):
+            xr.testing.assert_equal(data_zarr, data_tiff_3d)
+        else:
+            xr.testing.assert_allclose(data_zarr, data_tiff_3d)
+
+
+def test_3d_dask_chunks(index, indexed_gedi_gtif, indexed_gedi_zarr):
+    """Test dask load."""
+
+    dc = Datacube(index=index)
+    prod = "gedi_l2b_cover_z"
+    measurement = "cover_z"
+
+    data_tiff = dc.load(
+        product=prod,
+        latitude=LBG_LATITUDE,
+        longitude=LBG_LONGITUDE,
+        output_crs=GEDI_CRS,
+        resolution=GEDI_RESOLUTION,
+        dask_chunks={'time': 2},
+    )
+    data_tiff_3d = stack_3d_on_z(data_tiff, measurement)
+    data_zarr = dc.load(
+        product=f"{prod}_zarr",
+        measurements=[measurement],
+        latitude=LBG_LATITUDE,
+        longitude=LBG_LONGITUDE,
+        output_crs=GEDI_CRS,
+        resolution=GEDI_RESOLUTION,
+        dask_chunks={'time': 2, 'z': 15},
+    )
+    assert data_zarr[measurement].size
+    xr.testing.assert_equal(data_zarr, data_tiff_3d)
+
+
+def test_3d_groupby_solar_day(index, indexed_gedi_gtif, indexed_gedi_zarr):
+    """Test load with 'group_by'."""
+    dc = Datacube(index=index)
+    prod = "gedi_l2b_cover_z"
+    measurement = "cover_z"
+
+    data_tiff = dc.load(
+        product=prod,
+        latitude=LBG_LATITUDE,
+        longitude=LBG_LONGITUDE,
+        output_crs=GEDI_CRS,
+        resolution=GEDI_RESOLUTION,
+        group_by='solar_day',
+    )
+    data_tiff_3d = stack_3d_on_z(data_tiff, measurement)
+    data_zarr = dc.load(
+        product=f"{prod}_zarr",
+        measurements=[measurement],
+        latitude=LBG_LATITUDE,
+        longitude=LBG_LONGITUDE,
+        output_crs=GEDI_CRS,
+        resolution=GEDI_RESOLUTION,
+        group_by='solar_day',
+    )
+    assert data_zarr[measurement].size
+    xr.testing.assert_equal(data_zarr, data_tiff_3d)
+
+
+def test_3d_fuse_func(index, indexed_gedi_gtif, indexed_gedi_zarr):
+    """Test load with 'fuse_func'."""
+    dc = Datacube(index=index)
+    prod = "gedi_l2b_cover_z"
+    measurement = "cover_z"
+
+    data_tiff = dc.load(
+        product=prod,
+        latitude=LBG_LATITUDE,
+        longitude=LBG_LONGITUDE,
+        output_crs=GEDI_CRS,
+        resolution=GEDI_RESOLUTION,
+        fuse_func=custom_dumb_fuser,
+    )
+    data_tiff_3d = stack_3d_on_z(data_tiff, measurement)
+    data_zarr = dc.load(
+        product=f"{prod}_zarr",
+        measurements=[measurement],
+        latitude=LBG_LATITUDE,
+        longitude=LBG_LONGITUDE,
+        output_crs=GEDI_CRS,
+        resolution=GEDI_RESOLUTION,
+        fuse_func=custom_dumb_fuser,
+    )
+    assert data_zarr[measurement].size
+    xr.testing.assert_equal(data_zarr, data_tiff_3d)
